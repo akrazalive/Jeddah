e384db203c86c14c126063918d59ce3c1d354dd8
akrazalive <akrazalive@gmail.com>
2026-04-13 13:49:36 +0500

    changes done

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ar" dir="rtl" className={`${inter.variable} ${cairo.variable}`}>
-      <body>
+      <body suppressHydrationWarning>
         <LangProvider>
           {children}
         </LangProvider>
